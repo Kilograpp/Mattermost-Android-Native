22ed6a3d24ffdcd3c4919cbadb87698bae66f95a
kepar <ilyichov13@mail.ru>
2016-10-14 11:21:47 +0300

    Before merge

--- app/src/main/java/com/kilogramm/mattermost/model/entity/filetoattacth/FileToAttachRepository.java
+++ app/src/main/java/com/kilogramm/mattermost/model/entity/filetoattacth/FileToAttachRepository.java
@@ -132,7 +132,6 @@ public class FileToAttachRepository implements Repository<FileToAttach>{
                     .findAll();
             fileToAttachList.deleteFirstFromRealm();
         });
-
         realm.close();
     }
 
