f06457bd0caf1a0dc867402866a66281e4927481
ngers <n.ger.s@ya.ru>
2016-11-10 11:38:56 +0300

    Add max lines edit text

--- app/src/main/java/com/kilogramm/mattermost/view/channel/ChannelActivity.java
+++ app/src/main/java/com/kilogramm/mattermost/view/channel/ChannelActivity.java
@@ -7,6 +7,7 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
