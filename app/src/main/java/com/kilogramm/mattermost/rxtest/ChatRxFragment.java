a0143d4079109a69444b7c745005d1d11882aa5b
kepar <ilyichov13@mail.ru>
2016-11-11 15:39:51 +0300

    Return downloaded state of files

--- app/src/main/java/com/kilogramm/mattermost/rxtest/ChatRxFragment.java
+++ app/src/main/java/com/kilogramm/mattermost/rxtest/ChatRxFragment.java
@@ -203,9 +203,9 @@ public class ChatRxFragment extends BaseFragment<ChatRxPresenter> implements OnI
     public void onResume() {
         super.onResume();
         setupToolbar("", channelName, v -> {
-            RealmResults<User> users = UserRepository.query(new UserByChannelIdSpecification(channelId));
+/*            RealmResults<User> users = UserRepository.query(new UserByChannelIdSpecification(channelId));
             if (users != null) ProfileRxActivity.start(getActivity(), users.first().getId());
-            else ChannelActivity.start(getActivity(), channelId);
+            else*/ ChannelActivity.start(getActivity(), channelId);
         }, v -> searchMessage());
         checkNeededPermissions();
     }
@@ -425,8 +425,22 @@ public class ChatRxFragment extends BaseFragment<ChatRxPresenter> implements OnI
         // post.setId(String.format("%s:%s", post.getUserId(), post.getCreateAt()));
         post.setFilenames(binding.attachedFilesLayout.getAttachedFiles());
         post.setPendingPostId(String.format("%s:%s", post.getUserId(), post.getCreateAt()));
-        if (post.getMessage().length() != 0 || (!FileToAttachRepository.getInstance().getFilesForAttach().isEmpty()
-                && !FileToAttachRepository.getInstance().haveUnloadedFiles())) {
+
+        /*
+        * post.getMessage().length() != 0 && FileToAttachRepository.getInstance().getFilesForAttach().isEmpty()
+        * post.getMessage().length() == 0 && !FileToAttachRepository.getInstance().getFilesForAttach().isEmpty() && !FileToAttachRepository.getInstance().haveUnloadedFiles()
+        * post.getMessage().length() != 0 && !FileToAttachRepository.getInstance().getFilesForAttach().isEmpty() && !FileToAttachRepository.getInstance().haveUnloadedFiles()
+        * */
+
+
+
+       /* if (post.getMessage().length() != 0 || (!FileToAttachRepository.getInstance().getFilesForAttach().isEmpty()
+                && !FileToAttachRepository.getInstance().haveUnloadedFiles())) {*/
+        if (
+        post.getMessage().length() != 0 && FileToAttachRepository.getInstance().getFilesForAttach().isEmpty() ||
+        post.getMessage().length() == 0 && !FileToAttachRepository.getInstance().getFilesForAttach().isEmpty() && !FileToAttachRepository.getInstance().haveUnloadedFiles() ||
+        post.getMessage().length() != 0 && !FileToAttachRepository.getInstance().getFilesForAttach().isEmpty() && !FileToAttachRepository.getInstance().haveUnloadedFiles()
+        ) {
             getPresenter().requestSendToServer(post);
             hideAttachedFilesLayout();
             //WebSocketService.with(context).sendTyping(channelId, teamId.getId());
--- app/src/main/java/com/kilogramm/mattermost/view/channel/ChannelActivity.java
+++ app/src/main/java/com/kilogramm/mattermost/view/channel/ChannelActivity.java
@@ -46,10 +46,10 @@ public class ChannelActivity extends BaseActivity<ChannelPresenter> implements V
         channelId = getIntent().getStringExtra(CHANNEL_ID);
 
         binding = DataBindingUtil.setContentView(this, R.layout.channel_activity);
-        initView();
+/*        initView();
         setToolbar();
         initClick();
-        setCTollBarTitle(getPresenter().getChannel().getDisplayName());
+        setCTollBarTitle(getPresenter().getChannel().getDisplayName());*/
 
     }
 
@@ -143,9 +143,9 @@ public class ChannelActivity extends BaseActivity<ChannelPresenter> implements V
                 this,
                 id -> openDirect(id),
                 extraInfo.getMembers(), true);
-        binding.list.setAdapter(allMembersAdapter);
-        binding.list.setLayoutManager(new LinearLayoutManager(this));
-        binding.list.setNestedScrollingEnabled(false);
+//        binding.list.setAdapter(allMembersAdapter);
+//        binding.list.setLayoutManager(new LinearLayoutManager(this));
+//        binding.list.setNestedScrollingEnabled(false);
     }
 
 
