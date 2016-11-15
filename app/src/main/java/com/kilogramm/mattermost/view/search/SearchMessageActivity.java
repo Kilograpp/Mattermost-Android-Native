30a3e702858a60b1923355538303b0406988dd60
melkshake <melkshake@kilograpp.com>
2016-11-15 17:20:31 +0300

    search history done

--- app/src/main/java/com/kilogramm/mattermost/view/search/SearchMessageActivity.java
+++ app/src/main/java/com/kilogramm/mattermost/view/search/SearchMessageActivity.java
@@ -1,13 +1,21 @@
 package com.kilogramm.mattermost.view.search;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Display;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +25,12 @@ import com.kilogramm.mattermost.model.entity.FoundMessagesIds;
 import com.kilogramm.mattermost.model.entity.post.Post;
 import com.kilogramm.mattermost.presenter.SearchMessagePresenter;
 import com.kilogramm.mattermost.view.BaseActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmQuery;
@@ -30,18 +44,24 @@ import nucleus.factory.RequiresPresenter;
 
 @RequiresPresenter(SearchMessagePresenter.class)
 public class SearchMessageActivity extends BaseActivity<SearchMessagePresenter>
-                                   implements TextView.OnEditorActionListener, SearchMessageAdapter.OnJumpClickListener {
+                                   implements TextView.OnEditorActionListener,
+                                              SearchMessageAdapter.OnJumpClickListener,
+                                              TextWatcher {
 
     private static final String TEAM_ID = "team_id";
     public static final String MESSAGE_ID = "message_id";
     public static final String CHANNEL_ID = "channel_id";
     public static final String CHANNEL_NAME = "channel_name";
     public static final String TYPE_CHANNEL = "type_channel";
+    public static final String SEARCH_HISTORY = "search_history";
 
     private ActivitySearchBinding binding;
     private SearchMessageAdapter adapter;
 
     String terms;
+
+    ArrayAdapter<String> historyAutoCompleteAdapter;
+    ArrayList<String> historyAutoComplete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +71,25 @@ public class SearchMessageActivity extends BaseActivity<SearchMessagePresenter>
 
     public void init() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
+        binding.searchAutoComplete.setOnEditorActionListener(this);
+        binding.searchAutoComplete.addTextChangedListener(this);
 
-        binding.searchText.setOnEditorActionListener(this);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        binding.searchAutoComplete.setDropDownWidth(size.x);
+
+        historyAutoComplete = new ArrayList<>();
+        historyAutoComplete = getSearchHistory(getApplication(), SEARCH_HISTORY);
+        historyAutoCompleteAdapter = new ArrayAdapter<>(
+                SearchMessageActivity.this,
+                R.layout.item_search_history_dropdown,
+                historyAutoComplete);
+        binding.searchAutoComplete.setAdapter(historyAutoCompleteAdapter);
+
         binding.btnBack.setOnClickListener(v -> finish());
         binding.btnClear.setOnClickListener(v -> {
-            binding.searchText.setText("");
+            binding.searchAutoComplete.setText("");
             defaultMessageVisibility(true);
             searchResultVisibility(false);
             defaultVisibility(false);
@@ -85,10 +119,16 @@ public class SearchMessageActivity extends BaseActivity<SearchMessagePresenter>
     }
 
     public void doMessageSearch() {
-        terms = binding.searchText.getText().toString();
+        terms = binding.searchAutoComplete.getText().toString();
+
         if (terms.equals("")) {
             Toast.makeText(this, this.getResources().getString(R.string.empty_search), Toast.LENGTH_SHORT).show();
         } else {
+            if (!historyAutoComplete.contains(terms)) {
+                historyAutoCompleteAdapter.add(terms);
+                historyAutoComplete.add(terms);
+            }
+            setSearchHistory(getApplication(), SEARCH_HISTORY, historyAutoComplete);
             getPresenter().search(terms);
         }
     }
@@ -134,5 +174,52 @@ public class SearchMessageActivity extends BaseActivity<SearchMessagePresenter>
         Intent starter = new Intent(context, SearchMessageActivity.class);
         starter.putExtra(TEAM_ID, teamId);
         context.startActivityForResult(starter,id);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+
+    private void setSearchHistory(Context context, String key, ArrayList<String> history) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        JSONArray jsonArray = new JSONArray();
+
+        for (String item : history) {
+            jsonArray.put(item);
+        }
+        if (!history.isEmpty()) {
+            editor.putString(key, jsonArray.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.commit();
+    }
+
+    private ArrayList<String> getSearchHistory (Context context, String key) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = preferences.getString(key, null);
+        ArrayList<String> storedHistory = new ArrayList<>();
+
+        if (json != null) {
+            try {
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String historyItem = jsonArray.optString(i);
+                    storedHistory.add(historyItem);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return storedHistory;
     }
 }
