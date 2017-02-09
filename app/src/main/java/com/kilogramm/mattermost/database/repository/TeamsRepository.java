7199a011c0911ffc952239a4325f1c4c1a86d70f
Evgeny Suetin <evgenysuetin@kilograpp.com>
2017-02-09 14:55:48 +0300

    add sqlite to login screen

--- app/src/main/java/com/kilogramm/mattermost/database/repository/TeamsRepository.java
+++ app/src/main/java/com/kilogramm/mattermost/database/repository/TeamsRepository.java
@@ -1,5 +1,15 @@
 package com.kilogramm.mattermost.database.repository;
 
+import android.content.ContentValues;
+
+import com.kilogramm.mattermost.MattermostApp;
+import com.kilogramm.mattermost.database.DBHelper;
+import com.kilogramm.mattermost.database.MattermostContentProvider;
+import com.kilogramm.mattermost.model.entity.team.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by kepar on 06.02.17.
  */
@@ -9,4 +19,21 @@ public class TeamsRepository {
     public static final String TABLE_NAME_TEAMS = "Teams";
 
     public static final String FIELD_NAME = "name";
+
+    public static void addTeam(List<Team> teams) {
+        List<ContentValues> list = new ArrayList<>();
+        for (Team team : teams) {
+            ContentValues values = new ContentValues();
+            values.put(DBHelper.FIELD_COMMON_ID, team.getId());
+            values.put(FIELD_NAME, team.getDisplayName());
+            list.add(values);
+        }
+        ContentValues[] contentValues = new ContentValues[teams.size()];
+        list.toArray(contentValues);
+        MattermostApp.getSingleton()
+                .getApplicationContext()
+                .getContentResolver()
+                .bulkInsert(MattermostContentProvider.CONTENT_URI_TEAMS, contentValues);
+    }
+
 }
--- app/src/main/java/com/kilogramm/mattermost/database/repository/UsersRepository.java
+++ app/src/main/java/com/kilogramm/mattermost/database/repository/UsersRepository.java
@@ -1,5 +1,12 @@
 package com.kilogramm.mattermost.database.repository;
 
+import android.content.ContentValues;
+
+import com.kilogramm.mattermost.MattermostApp;
+import com.kilogramm.mattermost.database.DBHelper;
+import com.kilogramm.mattermost.database.MattermostContentProvider;
+import com.kilogramm.mattermost.model.entity.user.User;
+
 /**
  * Created by kepar on 06.02.17.
  */
@@ -19,4 +26,25 @@ public class UsersRepository {
     public static final String FIELD_STATUS = "status";
     public static final String FIELD_IN_TEAM = "inTeam";
     public static final String FIELD_IS_SHOW = "isShow";
+
+    public static void addUser(User user){
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.FIELD_COMMON_ID, user.getId());
+        values.put(FIELD_USER_NAME, user.getUsername());
+        values.put(FIELD_NICK_NAME, user.getNickname());
+        values.put(FIELD_EMAIL, user.getEmail());
+        values.put(FIELD_FIRST_NAME, user.getFirstName());
+        values.put(FIELD_LAST_NAME, user.getLastName());
+        values.put(FIELD_UPDATED_AT, user.getUpdateAt());
+        values.put(FIELD_LAST_ACTIVITY_AT, user.getLastActivityAt());
+        values.put(FIELD_LAST_PICTURE_UPDATE, user.getLastPictureUpdate());
+        values.put(FIELD_STATUS, user.getStatus());
+       /* values.put(FIELD_IN_TEAM, user.getInTeam());
+        values.put(FIELD_IS_SHOW, user.get());*/
+        MattermostApp.getSingleton()
+                .getApplicationContext()
+                .getContentResolver()
+                .insert(MattermostContentProvider.CONTENT_URI_USERS, values);
+    }
+
 }
